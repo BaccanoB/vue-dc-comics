f9dbee7c6b74cc62d8e7394622a5850b0fe18949
BaccanoB <beatrice.baccano@yahoo.it>
2021-06-09 14:47:52 +0200

    aggiunto file general

--- src/App.vue
+++ src/App.vue
@@ -21,15 +21,5 @@
 </script>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&display=swap');
-
-  body {
-    font-family: 'Open Sans Condensed', sans-serif;
-  }
-
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+@import './style/general';
 </style>
